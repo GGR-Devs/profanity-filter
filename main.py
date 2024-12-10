bc6c3a38ecb3470254d638e0e7004bd43e551c42
Bence <78733248+BXn4@users.noreply.github.com>
2024-12-10 20:58:46 +0100

    Quick fixes, directory creation

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ logger = logging.getLogger(__name__)
 
 def check():
     logger.info('Checking if the dataset is exist...')
-    if not os.listdir(dataset_path):
+    if not os.path.exists(dataset_path):
         logger.warning('Dataset does not exist!')
+        logger.info('Creating dataset directory...')
+        os.makedirs(dataset_path)
         for file_name in os.listdir(words_path):
             file_path = os.path.join(words_path, file_name)
             if os.path.isfile(file_path):
@@ -31,6 +33,9 @@ def load_dataset(path: str):
 
 
 if __name__ == '__main__':
+    if not os.path.exists(logs_path):
+        os.makedirs(logs_path)
+        
     logging.basicConfig(filename=f'{logs_path}/log.log', level=logging.DEBUG)
     logger.info('Started')
 
--- utils/convert.py
+++ utils/convert.py
@@ -6,7 +6,7 @@ def from_source_to_csv(input_path: str, output_path: str):
     filename = os.path.splitext(os.path.basename(input_path).upper())[0]
     language = filename
 
-    with open(input_path, 'r') as input_file:
+    with open(input_path, 'r', encoding='utf-8') as input_file:
         lines = input_file.read().splitlines()
 
         stripped = [line.strip() for line in lines if line.strip()]
@@ -14,7 +14,7 @@ def from_source_to_csv(input_path: str, output_path: str):
         rows = [(index, language, word) for index, word in enumerate(stripped)]
 
         file_exists = os.path.exists(output_path)
-        with open(output_path, 'a', newline='') as out_file:
+        with open(output_path, 'a', newline='', encoding='utf-8') as out_file:
             writer = csv.writer(out_file)
             if not file_exists:
                 writer.writerow(('index', 'language', 'word'))
